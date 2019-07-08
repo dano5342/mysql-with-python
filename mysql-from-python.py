2bea682a7b314082e7a83b9f7303b8cd2be6c50a
Ubuntu <ubuntu@ip-172-31-94-69.ec2.internal>
2019-07-08 10:24:16 +0000

    Have gone through using PYthon and MYSQL together running through different command structures and how to use a Where Clause

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,7 +1,8 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -12,12 +13,11 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ["Fred", "Freddie"]
+        # Prepare a string with the same number of placeholders in a list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit()
 finally:
-    # Close the connection, regardless of whether or not the above was successful
     connection.close()
